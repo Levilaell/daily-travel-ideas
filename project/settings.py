0f016c7cd6dddfe819cec3e761a89a43a8a02597
Levilaell <levilael2@hotmail.com>
2024-11-15 00:58:36 -0300

    first commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r@ip2vk=qv6w4vy-rir9lz*n01x6slr0e*f9ht5h(p!-yva$sj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
